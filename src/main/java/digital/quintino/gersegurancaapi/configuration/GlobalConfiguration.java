2d3d2bc40294cc2dd83632c3279f802cc89fa402
Desenvolvimento - Quintino Digital <desenvolvimento@quintino.digital>
2022-12-04 19:36:34 -0300

    :large_blue_circle: Feature: Configurar JWT

---
+++ src/main/java/digital/quintino/gersegurancaapi/configuration/GlobalConfiguration.java
@@ -0,0 +1,12 @@
+package digital.quintino.gersegurancaapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class GlobalConfiguration {
+
+    
+
+}
